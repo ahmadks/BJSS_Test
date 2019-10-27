590e8f2194bc73d31292f062059436b343da6ea1
Ahmad Kamal <ahmadks@gmail.com>
2019-10-27 15:33:11 +0000

    Offer date validation

--- src/main/java/com/bjss/service/DispatcherService.java
+++ src/main/java/com/bjss/service/DispatcherService.java
@@ -2,6 +2,7 @@ package com.bjss.service;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map.Entry;
@@ -31,15 +32,17 @@ public class DispatcherService {
 	}
 
 	public String getTotal() {
-		if(subTotal == null) calculateSubTotal();
-		if(discount == null) calculateDiscount();
+		if (subTotal == null)
+			calculateSubTotal();
+		if (discount == null)
+			calculateDiscount();
 		this.total = subTotal - discount;
-		
+
 		return "Total: " + getFormattedCurrency(total);
 	}
 
 	public String calculateSubTotal() {
-		this.subTotal=0f;
+		this.subTotal = 0f;
 
 		for (Entry<String, Integer> gd : basket.getBasket().entrySet()) {
 			Good good = goods.getGoodsMap().get(gd.getKey());
@@ -52,21 +55,25 @@ public class DispatcherService {
 
 	public List<String> calculateDiscount() {
 		List<String> respMsg = new ArrayList<String>();
-		this.discount=0f;
+		this.discount = 0f;
 		for (Entry<String, Offer> of : offers.getOffersMap().entrySet()) {
-			Integer basketUnit = basket.getBasket().get(of.getKey());
-			if (basketUnit != null && basketUnit >= of.getValue().getRequiredUnits()) {
-				String applyFor = of.getValue().getApplyFor();
-				Integer goodUnitDis = basket.getBasket().get(applyFor);
-				Integer unitDis = basketUnit / of.getValue().getRequiredUnits();
-				if (goodUnitDis >= unitDis) {
-					String msg = applyFor.substring(0, 1).toUpperCase() + applyFor.substring(1) + " "
-							+ (int) (of.getValue().getDiscount() * 100 )+ "% off: -";
-					Good good = goods.getGoodsMap().get(applyFor);
-					
-					float disc = good.getPrice() * unitDis * of.getValue().getDiscount();
-					this.discount += disc;
-					respMsg.add(msg.concat(getFormattedCurrency(disc)));
+			Date today = new Date();
+			if (today.after(of.getValue().getValidFrom()) && today.before(of.getValue().getValidTo())) {
+				Integer basketUnit = basket.getBasket().get(of.getKey());
+				if (basketUnit != null && basketUnit >= of.getValue().getRequiredUnits()) {
+					String applyFor = of.getValue().getApplyFor();
+					Integer goodUnitDis = basket.getBasket().get(applyFor);
+					Integer unitDis = basketUnit / of.getValue().getRequiredUnits();
+					if (goodUnitDis !=null) {
+						unitDis = Math.min(goodUnitDis, unitDis);
+						String msg = applyFor.substring(0, 1).toUpperCase() + applyFor.substring(1) + " "
+								+ (int) (of.getValue().getDiscount() * 100) + "% off: -";
+						Good good = goods.getGoodsMap().get(applyFor);
+
+						float disc = good.getPrice() * unitDis * of.getValue().getDiscount();
+						this.discount += disc;
+						respMsg.add(msg.concat(getFormattedCurrency(disc)));
+					}
 				}
 			}
 
@@ -77,8 +84,8 @@ public class DispatcherService {
 	}
 
 	private String getFormattedCurrency(Float balance) {
-		if (balance<1)
-			return (int) (balance * 100) +"p";
+		if (balance < 1)
+			return (int) (balance * 100) + "p";
 		return DecimalFormat.getCurrencyInstance(Locale.UK).format(balance);
 	}
 }
